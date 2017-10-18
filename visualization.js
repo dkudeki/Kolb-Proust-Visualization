70cd7827146c23abaf93d562438fd2b275ccd203
Deren Kudeki <dkudeki@illinois.edu>
2017-10-18 15:05:16 -0500

    Basic structure for date slider added

--- visualization.js
+++ visualization.js
@@ -148,7 +148,56 @@ d3.json("coocurrences_family.json", function(error, graph) {
 		displayNetwork(setFocus(work_graph,selectValue));
 	}
 
+	//Set date range. Widest is 1633-1991
+
 	displayNetwork(setFocus(work_graph,center_family));
+
+/*	var x = d3.scaleLinear()
+ 		.domain([1880, 1930])
+ 		.range([0, width-50])
+		.clamp(true);
+
+	var slider = svg.append("g")
+		.attr("class", "slider")
+		.attr("transform", "translate(" + 25 + "," + 570 + ")");
+
+	slider.append("line")
+		.attr("class", "track")
+		.attr("x1", x.range()[0])
+		.attr("x2", x.range()[1])
+		.select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+			.attr("class", "track-inset")
+		.select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+			.attr("class", "track-overlay")
+		.call(d3.drag()
+			.on("start.interrupt", function() { slider.interrupt(); })
+			.on("start drag", function() { hue(x.invert(d3.event.x)); }));
+
+	slider.insert("g", ".track-overlay")
+		.attr("class", "ticks")
+		.attr("transform", "translate(0," + 18 + ")")
+		.selectAll("text")
+			.data(x.ticks(10))
+			.enter().append("text")
+			.attr("x", x)
+			.attr("text-anchor", "middle")
+			.text(function(d) { return d; });
+
+	var handle = slider.insert("circle", ".track-overlay")
+		.attr("class", "handle")
+		.attr("r", 9);
+
+	slider.transition() // Gratuitous intro!
+		.duration(750)
+		.tween("hue", function() {
+			var i = d3.interpolate(0, 70);
+			return function(t) { hue(i(t)); };
+		});
+
+	function hue(h) {
+		handle.attr("cx", x(h));
+		svg.style("background-color", d3.hsl(h, 0.8, 0.8));
+	}*/
 
 	function displayNetwork(display_graph) {
 		var link = svg.append("g")
